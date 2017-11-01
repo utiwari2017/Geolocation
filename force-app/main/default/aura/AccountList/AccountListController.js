294424e3c8b14d58d0fbc37eb96812c97eed1961
utiwari <utiwari@users.noreply.github.com>
2017-10-31 23:59:05 -0700

    added source code

---
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -0,0 +1,13 @@
+({
+    doInit : function(component, event) {
+        var action = component.get("c.findAll");
+        action.setCallback(this, function(a) {
+          component.set("v.accounts", a.getReturnValue()); 
+        });
+        $A.enqueueAction(action);
+    }
+})({
+    myAction : function(component, event, helper) {
+
+    }
+})
